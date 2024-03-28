0120a978dab27c15362df81aebd77884d6c5358e
fjktkm <fjktkm@gmail.com>
2024-03-28 14:39:36 +0900

    chore: HUGO に変更

--- lib/markdown.ts
+++
@@ -1,8 +0,0 @@
-// lib/markdown.ts
-import { remark } from 'remark';
-import html from 'remark-html';
-
-export async function markdownToHtml(markdown: string): Promise<string> {
-    const result = await remark().use(html).process(markdown);
-    return result.toString();
-}
